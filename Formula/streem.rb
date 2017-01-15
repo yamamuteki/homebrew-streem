952542f8695ed4f5d15f35e9cea9a1de65c5b2c4
yama@muteki <yama@muteki.com>
2017-01-15 20:12:16 +0900

    Run brew audit

--- Formula/streem.rb
+++ Formula/streem.rb
@@ -1,38 +1,16 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Streem < Formula
-  desc ""
-  homepage ""
+  desc "prototype of stream based programming language"
+  homepage "https://github.com/matz/streem"
   url "https://github.com/matz/streem/archive/201611.tar.gz"
   sha256 "807a9ccf3162efb061a15a333943050b329f0232cceb55492e5cd582c09a7fb4"
-
-  # depends_on "cmake" => :build
+  head "https://github.com/matz/streem.git"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    # system "./configure", "--disable-debug",
-    #                       "--disable-dependency-tracking",
-    #                       "--disable-silent-rules",
-    #                       "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make"#, "install" # if this fails, try separate make/make install steps
-    system "cp", "-r", "bin", "#{prefix}"
+    system "make"
+    cp_r "bin", "#{prefix}"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test streem`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
